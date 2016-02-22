c144164212a64885b5a638470eeebbaf916964fb
Duarte Cunha Leão <duarte.leao@gmail.com>
2016-02-22 22:59:17 +0000

    [BACKLOG-5807] Fixes

--- package-res/resources/web/common-ui-require-js-cfg.js
+++ package-res/resources/web/common-ui-require-js-cfg.js
@@ -192,16 +192,6 @@
   requirePaths["common-ui/angular-directives"] = basePath + "/angular-directives";
   requireShim ["common-ui/angular-directives"] = ["common-ui/angular-ui-bootstrap"];
 
-  // Visual API
-  requireService["pentaho/visual/config"] = "IVisualApiConfiguration";
-
-  // Visual API - CCC
-  requireService["pentaho/visual/ccc/visualTypeProvider"] = "IVisualTypeProvider";
-  requireService["pentaho/visual/ccc/visualApiConfig"] = "IVisualApiConfiguration";
-
-  // Sample visualizations
-  requireService["pentaho/visual/samples/calc/visualTypeProvider"] = "IVisualTypeProvider";
-
   // CCC Theme
   requireMap["*"]["pentaho/visual/ccc/abstract/themes"] =
     "pentaho/visual/ccc/abstract/themes/" +
@@ -215,7 +205,7 @@
     });
   }
 
-  registerPackage("pentaho/visual/base", "modelFactory");
+  registerPackage("pentaho/visual/base", "model");
   registerPackage("pentaho/visual/ccc/abstract", "model");
   registerPackage("pentaho/visual/ccc/bar", "model");
 
